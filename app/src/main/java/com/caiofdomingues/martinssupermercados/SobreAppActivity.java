d2fb30eab17b1a9ac1266e912403f3367d7d9fcc
Caio F. Domingues <caiofelipe502@gmail.com>
2017-06-08 20:57:23 -0300

    ultimas mudancas

--- app/src/main/java/com/caiofdomingues/martinssupermercados/SobreAppActivity.java
+++ app/src/main/java/com/caiofdomingues/martinssupermercados/SobreAppActivity.java
@@ -1,7 +1,10 @@
 package com.caiofdomingues.martinssupermercados;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SobreAppActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class SobreAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre_app);
+
+        Button btnchangelog = (Button) findViewById(R.id.btnchangelog);
+
+        btnchangelog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(SobreAppActivity.this, ChangelogActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
